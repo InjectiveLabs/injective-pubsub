cd5a4c2d8af0fc58ace83a16cd01068befa88a76
Hilari Moragrega <hilari@hilarimoragrega.com>
2023-07-19 17:34:55 +0200

    feat(pubsub) prevent closing the subscriber channels multiple times

--- go.mod
+++ go.mod
@@ -4,9 +4,6 @@ go 1.20
 
 require (
 	github.com/InjectiveLabs/metrics v0.0.5
-	github.com/InjectiveLabs/suplog v1.3.3
-	github.com/avast/retry-go v3.0.0+incompatible
-	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.4
 )
 
@@ -22,10 +19,12 @@ require (
 	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/xlab/closer v0.0.0-20190328110542-03326addb7c2 // indirect
 	github.com/xlab/suplog v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20220517181318-183a9ca12b87 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -1,11 +1,18 @@
 package pubsub
 
 import (
+	"errors"
+	"fmt"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/InjectiveLabs/metrics"
-	"github.com/pkg/errors"
+)
+
+var (
+	ErrTopicNotFound      = errors.New("topic not found")
+	ErrSubscriberNotFound = errors.New("subscriber not found")
 )
 
 type EventBus interface {
@@ -22,12 +29,33 @@ type EventSubscriber interface {
 	EventUnsubscribe(topic string, subID int) error
 }
 
-type memEventBus struct {
+type subscriber struct {
+	ch     chan<- interface{}
+	closed uint32
+}
+
+func (s *subscriber) close() {
+	if atomic.SwapUint32(&s.closed, 1) == 1 {
+		return
+	}
+	close(s.ch)
+	s.ch = nil
+}
+
+func (s *subscriber) send(msg interface{}) {
+	if atomic.LoadUint32(&s.closed) == 1 {
+		return
+	}
+
+	s.ch <- msg
+}
+
+type MemEventBus struct {
 	topics                 map[string]<-chan interface{}
 	topicsMux              *sync.RWMutex
-	subscribers            map[string]map[int]chan<- interface{}
+	subscribers            map[string]map[int]*subscriber
 	subscribersMux         *sync.RWMutex
-	ids                    uint64
+	ids                    int
 	slowSubscriberMaxDelay time.Duration
 
 	svcTags metrics.Tags
@@ -35,11 +63,11 @@ type memEventBus struct {
 
 func NewEventBus(
 	slowSubscriberMaxDelay time.Duration,
-) EventBus {
-	return &memEventBus{
+) *MemEventBus {
+	return &MemEventBus{
 		topics:                 make(map[string]<-chan interface{}),
 		topicsMux:              new(sync.RWMutex),
-		subscribers:            make(map[string]map[int]chan<- interface{}),
+		subscribers:            make(map[string]map[int]*subscriber),
 		subscribersMux:         new(sync.RWMutex),
 		slowSubscriberMaxDelay: slowSubscriberMaxDelay,
 		svcTags: metrics.Tags{
@@ -48,7 +76,7 @@ func NewEventBus(
 	}
 }
 
-func (m *memEventBus) Topics() (topics []string) {
+func (m *MemEventBus) Topics() (topics []string) {
 	m.topicsMux.RLock()
 	defer m.topicsMux.RUnlock()
 
@@ -60,7 +88,7 @@ func (m *memEventBus) Topics() (topics []string) {
 	return topics
 }
 
-func (m *memEventBus) AddTopic(name string, src <-chan interface{}) error {
+func (m *MemEventBus) AddTopic(name string, src <-chan interface{}) error {
 	m.topicsMux.Lock()
 	_, ok := m.topics[name]
 	if ok {
@@ -73,16 +101,14 @@ func (m *memEventBus) AddTopic(name string, src <-chan interface{}) error {
 	return nil
 }
 
-var errNoTopic = errors.New("topic not found")
-
-func (m *memEventBus) Subscribe(topic string) (<-chan interface{}, int, error) {
+func (m *MemEventBus) Subscribe(topic string) (<-chan interface{}, int, error) {
 
 	m.topicsMux.RLock()
 	_, ok := m.topics[topic]
 	m.topicsMux.RUnlock()
 
 	if !ok {
-		return nil, 0, errNoTopic
+		return nil, 0, ErrTopicNotFound
 	}
 
 	inCh := make(chan interface{})
@@ -91,75 +117,77 @@ func (m *memEventBus) Subscribe(topic string) (<-chan interface{}, int, error) {
 	defer m.subscribersMux.Unlock()
 
 	m.ids++
-	subID := int(m.ids)
-	if m.subscribers[topic] == nil {
-		m.subscribers[topic] = make(map[int]chan<- interface{})
+	if m.ids <= 0 {
+		m.ids = 1
 	}
-	m.subscribers[topic][subID] = inCh
+	subID := m.ids
+	if m.subscribers[topic] == nil {
+		m.subscribers[topic] = make(map[int]*subscriber)
+	}
+	s := &subscriber{ch: inCh}
+	m.subscribers[topic][subID] = s
 
 	// here we use a double channel to avoid blocking the publisher
 	go func() {
+		defer close(outCh)
 		metrics.ReportClosureFuncCall("subscriber_publish", m.svcTags)
 		doneFn := metrics.ReportClosureFuncTiming("subscriber_publish", m.svcTags)
 		defer doneFn()
 
 		for msg := range inCh {
 			timeout := time.NewTimer(m.slowSubscriberMaxDelay)
-			defer timeout.Stop()
 
 			select {
 			case outCh <- msg:
+				timeout.Stop()
 
 			case <-timeout.C:
+				timeout.Stop()
+
 				metrics.SlowSubscriberEventsDropped(1, m.svcTags)
-				close(outCh)
-				close(inCh)
+				s.close()
 				return
 			}
 		}
-		// close the output channel when the input channel is closed
-		close(outCh)
-		return
 	}()
 
 	return outCh, subID, nil
 }
 
-func (m *memEventBus) Unsubscribe(topic string, subID int) error {
+func (m *MemEventBus) Unsubscribe(topic string, subID int) error {
 	m.subscribersMux.Lock()
 	defer m.subscribersMux.Unlock()
 
 	topicSubscribers := m.subscribers[topic]
-	ch, ok := topicSubscribers[subID]
-	if ok {
-		delete(topicSubscribers, subID)
-		close(ch)
-	} else {
-		err := errors.Errorf("subscriber to %s with ID %d not found", topic, subID)
-		return err
+	s, ok := topicSubscribers[subID]
+	if !ok {
+		return fmt.Errorf("%w: to %s with ID %d", ErrSubscriberNotFound, topic, subID)
 	}
+
+	delete(topicSubscribers, subID)
+	s.close()
 
 	return nil
 }
 
-func (m *memEventBus) EventUnsubscribe(topic string, subID int) error {
+func (m *MemEventBus) EventUnsubscribe(topic string, subID int) error {
 	return m.Unsubscribe(topic, subID)
 }
 
 // EventSubscribe implements EventSubscriber
-func (m *memEventBus) EventSubscribe(topic string) (msgs <-chan interface{}, subID int, err error) {
+func (m *MemEventBus) EventSubscribe(topic string) (msgs <-chan interface{}, subID int, err error) {
 	return m.Subscribe(topic)
 }
 
-func (m *memEventBus) publishTopic(topic string, src <-chan interface{}) {
+func (m *MemEventBus) publishTopic(topic string, src <-chan interface{}) {
 	go func() {
 		for {
 			select {
 			case msg, ok := <-src:
 				if !ok {
-					m.closeAllSubscribers(topic)
 					m.topicsMux.Lock()
 					delete(m.topics, topic)
+					m.closeAllSubscribers(topic)
 					m.topicsMux.Unlock()
 					return
 				}
@@ -170,41 +198,22 @@ func (m *memEventBus) publishTopic(topic string, src <-chan interface{}) {
 	}()
 }
 
-func (m *memEventBus) closeAllSubscribers(topic string) {
+func (m *MemEventBus) closeAllSubscribers(topic string) {
 	m.subscribersMux.Lock()
-	defer m.subscribersMux.Unlock()
-
 	subscribers := m.subscribers[topic]
 	delete(m.subscribers, topic)
+	m.subscribersMux.Unlock()
 
 	for _, sub := range subscribers {
-		close(sub)
+		sub.close()
 	}
 }
 
-func copyMap(src map[int]chan<- interface{}) map[int]chan<- interface{} {
-	dst := make(map[int]chan<- interface{}, len(src))
-
-	for k, v := range src {
-		dst[k] = v
-	}
-
-	return dst
-}
-
-func (m *memEventBus) publishAllSubscribers(topic string, msg interface{}) {
-	var subscribers map[int]chan<- interface{}
-
+func (m *MemEventBus) publishAllSubscribers(topic string, msg interface{}) {
 	m.subscribersMux.RLock()
-	subscribers = copyMap(m.subscribers[topic])
-	m.subscribersMux.RUnlock()
+	defer m.subscribersMux.RUnlock()
 
-	// if there is no subscribers for this topic, simply drop msg
-	if len(subscribers) == 0 {
-		return
-	}
-
-	for _, sub := range subscribers {
-		sub <- msg
+	for _, sub := range m.subscribers[topic] {
+		sub.send(msg)
 	}
 }
